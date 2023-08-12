71cb6e58d523cc1193a4b092329a62e3c7331220
Agustin2799 <140901456+Agustin2799@users.noreply.github.com>
2023-08-12 10:43:03 -0300

    Solucionando problema de propagación.

--- eventos.js
+++ eventos.js
@@ -2,3 +2,8 @@ const div = document.getElementById("primerDiv");
 div.addEventListener("click", () => {
     alert("Hola soy el div");
 });
+const boton = document.getElementById("boton");
+boton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    alert("hola soy el boton");
+});
